677f96c7db865b3d8bf0c8ec70ff3a8a4456761c
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-10-18 15:46:29 +0200

    Fix init

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'django'
     ],
     extras_require={
         # eg:
--- src/zxcvbn_password/__init__.py
+++ src/zxcvbn_password/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
-__all__ = ['PasswordField', 'PasswordConfirmationField']
-
 from zxcvbn_password.fields import PasswordField, PasswordConfirmationField
 
+__all__ = ['PasswordField', 'PasswordConfirmationField']
 __version__ = '1.1.0'
-
