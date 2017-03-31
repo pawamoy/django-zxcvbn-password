36514fbe2ab1d6e737fb06c0d6fd0a1f984ac83c
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-03-31 16:58:53 +0200

    Update update.sh script

--- update.sh
+++ update.sh
@@ -7,26 +7,26 @@
 
   # Initialize paths
   project=$1
-  template=$2
-  message=${3:-Update template branch}
+  cookiecutter=$2
+  message=${3:-Update cookiecutter branch}
   abs_project="$(cd "${project}"; pwd)"
-  abs_template="$(cd "${template}"; pwd)"
+  abs_cookiecutter="$(cd "${cookiecutter}"; pwd)"
 
   echo "Absolute path to project:  ${abs_project}"
-  echo "Absolute path to template: ${abs_template}"
+  echo "Absolute path to cookiecutter: ${abs_cookiecutter}"
   echo "Commit message:            ${message}"
   echo
 
-  # Git checkout template branch
+  # Git checkout cookiecutter branch
   cd "${abs_project}"
-  echo "> git checkout template"
-  git checkout template >/dev/null 2>&1 || exit 1
+  echo "> git checkout cookiecutter"
+  git checkout cookiecutter >/dev/null 2>&1 || exit 1
 
   # Regenerate project
   echo "> regenerate cookiecutter"
   if ! cookiecutter --overwrite-if-exists --no-input \
                --config-file ".cookiecutterrc" \
-               --output-dir .. "${abs_template}" >/dev/null; then
+               --output-dir .. "${abs_cookiecutter}" >/dev/null; then
     echo
     echo "Problem during cookiecutter regeneration"
     exit 1
@@ -39,9 +39,8 @@
   echo "> git add, commit and push"
   git add . -A
   if git commit -m "${message}"; then
-    git push >/dev/null 2>&1
     echo
-    echo "Now checkout your main branch, merge template one,"
+    echo "Now checkout your main branch, merge cookiecutter one,"
     echo "fix conflicts if any then commit and push!"
   fi
   exit 0 # Force shell to exit here and not to read anything added after this compound command
